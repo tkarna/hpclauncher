052c8f332804394e163402c57e67ca8a2eb80079
Tuomas Karna <tuomas.karna@gmail.com>
2015-09-02 13:55:24 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='batchScriptLib',
+      version='0.0.1',
+      description='Interface for submitting jobs to HPC queue managers',
+      author='Tuomas Karna',
+      author_email='tuomas.karna@gmail.com',
+      url='https://bitbucket.org/tkarna/batchscriplib',
+      packages=['batchScriptLib'],
+      )
